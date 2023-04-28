52333ed928d8e67abc1719e49404645d8b304c38
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2023-04-28 11:09:16 +0200

    pruebas 1

--- Arbol.py
+++ Arbol.py
@@ -4,7 +4,6 @@ class Arbol():
         self.orden = orden
 
     def insertar_vuelo(self,vuelo):
-        print("HOLA")
         if self.raiz == None:
             self.raiz = Nodo(self.orden, self.orden[0])
         nodo:Nodo = self.raiz
@@ -12,21 +11,23 @@ class Arbol():
         posicion = 0
         for caracteristica in self.orden:
             atributo = getattr(vuelo, caracteristica)   # Atributo del vuelo
-            print(caracteristica, atributo, nodo)
             if atributo not in nodo.diccionario:
+                print(f"EL atributo {atributo} no está en el diccionario {nodo.diccionario} de {nodo}")
                 if posicion+1>=len(self.orden):
-                    nuevo_nodo = Nodo(self.orden)
+                    nuevo_nodo = Nodo(self.orden, None, {}, [])
                 else:
-                    nuevo_nodo = Nodo(self.orden, self.orden[posicion+1])
-
+                    nuevo_nodo = Nodo(self.orden, self.orden[posicion+1], {}, [])
                 nodo.diccionario[atributo] = nuevo_nodo
+                print(f"DESPUES -> {nodo.diccionario}")
+            
             nodo = nodo.diccionario[atributo]
 
             posicion = posicion + 1
         
         nodo.vuelos.append(vuelo)
-        
-
+        if len(nodo.vuelos) == 1:
+            print(nodo, nodo.vuelos)
+        return nodo
         # si no está, la añade al diccionario con un nodo
 
         # si está, cambia al nodo del diccionario y repite el proceso
@@ -37,3 +38,6 @@ class Nodo():
         self.caracteristica = caracteristica   # ["origen", "destino", "compania", "cuarto"]
         self.diccionario = diccionario
         self.vuelos = vuelos
+
+    def __repr__(self) -> str:
+        return f"{self.caracteristica}"
--- Vuelo.py
+++ Vuelo.py
@@ -5,3 +5,6 @@ class Vuelo:
         self.kilometros = kilometros
         self.compania = compania
         self.precio = precio
+
+    def __repr__(self) -> str:
+        return f"Vuelo(origen = {self.origen}, destino = {self.destino}, compañía = {self.compania}, precio = {self.precio}, km = {self.kilometros})"
